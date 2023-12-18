966116535316acf60c8697f4cf7308b9d195b023
CamrenColeman <123282989+CamrenColeman@users.noreply.github.com>
2023-12-17 17:14:29 -0800

    chore: add comments

--- Domain/Item.cs
+++ Domain/Item.cs
@@ -6,20 +6,28 @@ namespace Domain
     {
 
         [Key]
+        // Internal identifier for items (WCP-1017)
         public required string Sku { get; set; }
 
+        //Identical part at different with different SKU. (WCP-1234 at one vs A-4321 joined as PLY-0001)
         public string? SkuAlias { get; set; }
 
+        // Human readable name (HTD Timing Pulleys)
         public string? Name { get; set; }
 
+        //Number of items (6)
         public int Quantity { get; set; }
-        
+
+        //Type of item (Bearing, Pully)
         public required string Type { get; set; }
 
+        // Who sells the item (West Coast Production)
         public string? Vendor { get; set; }
-         
+
+        // URL to the item (https://wcproducts.com/collections/xxxxxxxxx)
         public string? Link { get; set; }
 
+        // What the object is made of (optional)
         public string? Material { get; set; }
 
     }
